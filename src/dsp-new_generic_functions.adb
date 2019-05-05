a0bf77bc3e12815c0ffec24960e057eb59a27b42
Riccardo Bernardini <riccardo.bernardini@uniud.it>
2019-05-05 12:37:42 +0200

    OK, now everything compiles and it is ready for test.

---
+++ src/dsp-new_generic_functions.adb
@@ -0,0 +1,69 @@
+pragma Ada_2012;
+with Ada.Numerics.Elementary_Functions;
+
+package body DSP.New_Generic_Functions is
+
+   function Complexify (X : Real_Filters.Coefficient_Array)
+                        return Complex_Filters.Coefficient_Array
+   is
+      Result : Complex_Filters.Coefficient_Array (X'Range);
+   begin
+      for K in X'Range loop
+         Result (K) := (X (K), 0.0);
+      end loop;
+
+      return Result;
+   end Complexify;
+
+
+
+
+   function Notch_Specs (Freq        : Normalized_Frequency;
+                         Pole_Radius : Float;
+                         Class       : Notch_Type := Stopband)
+                         return Complex_IIR_Spec
+   is
+      Tmp : constant Real_IIR_Spec := Notch_Specs (Freq, Pole_Radius, Class);
+   begin
+      return Complex_IIR_Spec'(Num_Deg     => Tmp.Num_Deg,
+                               Den_Deg     => Tmp.Den_Deg,
+                               Numerator   => Complexify (Tmp.Numerator),
+                               Denominator => Complexify (Tmp.Denominator));
+   end Notch_Specs;
+
+   function Notch_Specs (Freq        : Normalized_Frequency;
+                         Pole_Radius : Float;
+                         Class       : Notch_Type := Stopband)
+                         return Real_IIR_Spec
+   is
+      use Ada.Numerics;
+      use Ada.Numerics.Elementary_Functions;
+
+      C : constant Scalar_Type := Scalar_Type (2.0 * Cos (2.0 * Pi * Freq));
+      R : constant Scalar_Type := Scalar_Type (Pole_Radius);
+   begin
+      case Class is
+         when Stopband =>
+            return Real_IIR_Spec'(Num_Deg     => 2,
+                                  Den_Deg     => 2,
+                                  Numerator   =>
+                                    (0 => 1.0,
+                                     1 => -C,
+                                     2 => 1.0),
+
+                                  Denominator =>
+                                    (1 => -R * C,
+                                     2 => R ** 2));
+         when Passband =>
+            return Real_IIR_Spec'(Num_Deg     => 1,
+                                  Den_Deg     => 2,
+                                  Numerator   =>
+                                    (0 => C * (1.0 - R),
+                                     1 => R ** 2 - 1.0),
+
+                                  Denominator =>
+                                    (1 => -R * C,
+                                     2 => R ** 2));
+      end case;
+   end Notch_Specs;
+end DSP.New_Generic_Functions;
---
+++ src/dsp-new_generic_functions.ads
@@ -0,0 +1,66 @@
+with Ada.Numerics.Generic_Complex_Types;
+with Dsp.Ring_Filters;
+
+generic
+   type Scalar_Type is digits <>;
+
+   with package Complex_Types is
+     new Ada.Numerics.Generic_Complex_Types (Scalar_Type);
+package DSP.New_Generic_Functions is
+   subtype Complex_Type is Complex_Types.Complex;
+   use type Complex_Types.Complex;
+
+   package Real_Filters is
+     new Dsp.Ring_Filters (Sample_Type      => Scalar_Type,
+                           Coefficient_Type => Scalar_Type,
+                           One              => 1.0,
+                           Zero             => 0.0,
+                           Zero_Coeff       => 0.0);
+
+   function Delta_Signal (K : Integer) return Scalar_Type
+                          renames Real_Filters.Delta_Signal;
+
+   subtype Real_FIR is Real_Filters.Ring_FIR;
+
+   subtype Real_IIR is Real_Filters.Ring_IIR;
+
+   subtype Real_Filter_Interface is Real_Filters.Ring_Filter_Interface;
+
+   subtype Real_IIR_Spec is Real_Filters.Ring_IIR_Spec;
+
+
+   package Complex_Filters is
+     new Dsp.Ring_Filters (Sample_Type      => Complex_Type,
+                           Coefficient_Type => Complex_Type,
+                           One              => (1.0, 0.0),
+                           Zero             => (0.0, 0.0),
+                           Zero_Coeff       => (0.0, 0.0));
+
+   function Delta_Signal (K : Integer) return Complex_Type
+                          renames Complex_Filters.Delta_Signal;
+
+
+   subtype Complex_Filter_Interface is Complex_Filters.Ring_Filter_Interface;
+
+   subtype Complex_FIR is Complex_Filters.Ring_FIR;
+
+   subtype Complex_IIR is Complex_Filters.Ring_IIR;
+
+   subtype Complex_IIR_Spec is Complex_Filters.Ring_IIR_Spec;
+
+   type Notch_Type is (Passband, Stopband);
+
+   function Notch_Specs (Freq        : Normalized_Frequency;
+                         Pole_Radius : Float;
+                         Class       : Notch_Type := Stopband)
+                         return Complex_IIR_Spec;
+
+   function Notch_Specs (Freq        : Normalized_Frequency;
+                         Pole_Radius : Float;
+                         Class       : Notch_Type := Stopband)
+                         return Real_IIR_Spec;
+
+   function Complexify (X : Real_Filters.Coefficient_Array)
+                        return Complex_Filters.Coefficient_Array;
+
+end DSP.New_Generic_Functions;
